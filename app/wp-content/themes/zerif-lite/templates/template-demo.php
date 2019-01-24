4a94459806ddc12201c5d77c816cfefd18993014
Anibal Copitan <anibal@pprios.com>
2019-01-24 17:35:51 -0500

    [add]: new template for subscription 50

---
+++ app/wp-content/themes/zerif-lite/templates/template-demo.php
@@ -0,0 +1,27 @@
+<?php
+/* Template Name: Template Paymentfitnes One*/
+
+get_header();
+
+?>
+<div class="clear"></div>
+</header> <!-- / END HOME SECTION  -->
+
+
+<div class="site-content">
+	<div class="container" style="min-height: 1px;">
+		<div class="content-left-wrap col-md-9">
+			<article class="status-publish hentry">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+
+				<div class="entry-content">XXXXX
+					<?php echo get_the_content(); ?>
+				</div>
+			</article>
+		</div>
+
+	</div>
+</div>
+
+<?php
+get_footer();
